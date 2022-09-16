3b167eea710e8081e5ce23c1cc3eab621c58ea24
julietweswes <julietauma900@gmail.com>
2022-09-15 17:12:31 -0700

    Added all

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *  main -prints holberton
+ * main - Prints Holberton
  *
- *  Return: Always 0 (Sucess)
- *  /
-int main (void)
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-printf("-putchar\n");
-return (0);
+	printf("_putchar\n");
+	return (0);
 }
